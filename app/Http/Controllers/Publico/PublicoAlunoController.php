3deb5c4715fdccc710dd8dddc51a806292fe1b77
phlimasg <raphaelpc_@hotmail.com>
2020-11-10 10:00:09 -0300

    alteração de login

--- app/Http/Controllers/Publico/PublicoAlunoController.php
+++ app/Http/Controllers/Publico/PublicoAlunoController.php
@@ -160,7 +160,7 @@ class PublicoAlunoController extends Controller
                     'pAluno' => $aluno->ra
                     ]);
             }else{
-                $ra_liberado = RaLiberado::where('ra',$request->ra)->where('processo_id',$processo->id)->orderBy('created_at','asc')->first();
+                $ra_liberado = RaLiberado::where('ra',$request->ra)->where('processo_id',$processo->id)->latest()->first();
                 $aluno_processo = $processo->pAlunos->where('ra',$request->ra)->first();
 
                 
